2edbeae2a9b22ce50a74b58678a0d65c46af21cc
mogrady <martin.ogrady@zalando.de>
2017-01-08 18:19:28 +0100

    Adding some notes for better understanding of demo

--- src/app.js
+++ src/app.js
@@ -6,21 +6,30 @@ var scores = [
   { name: 'Emily', score: 88 }
 ];
 
+/**
+ Think of this as the intersection between the data and the dom elements.
+**/
 var update = d3.select('.chart')
   .selectAll('div')
   .data(scores, function (d) {
-    return d ? d.name : this.innerText;
+    return d ? d.name : this.innerText; //map name property to the innerText of the div.
   })
-  .style('color', 'blue');
+  .style('color', 'blue'); //If the name already exists within the list set it to blue. (intersection between data and the element).
 
+/**
+Enter refers to data but no dom elements.
+**/
 var enter = update.enter()
-  .append('div')
+  .append('div') // create div for everything that does not have a dom element.
   .text(function (d) {
     return d.name;
   })
   .style('color', 'green');
 
-update.exit().remove();
+/**
+Update refers to the dom elements already on the page.
+**/
+update.exit().remove(); // remove any data that is on the dom but does not have any corresponding data.
 
 update.merge(enter)
   .style('width', d => d.score + 'px')
